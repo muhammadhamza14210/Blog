ce91198c513cfd6cb674bdd391583f82d7cfa289
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-02-20 23:37:42 -0700

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -27,3 +28,33 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email ||!password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return next(errorHandler(404, "Invalid username or password"));
+    }
+
+    const isMatch = bcryptjs.compareSync(password, user.password);
+
+    if (!isMatch) {
+      return next(errorHandler(400, "Invalid username or password"));
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+    const {password:pass, ...rest} = user._doc;
+    res.status(200).cookie('access_token', token,{
+      httpOnly: true}).json(rest);
+    next();
+  } catch (error) {
+    next(error);
+  }
+}

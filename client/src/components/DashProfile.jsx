2fc8300756927fb0efdf3a888419bd84cb887f17
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-02-24 14:32:45 -0700

    Readability

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,24 +1,32 @@
 import { TextInput, Button, Alert } from "flowbite-react";
 import { useSelector } from "react-redux";
-import { useState, useRef,useEffect } from "react";
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import { app } from '../firebase';
-import { CircularProgressbar } from 'react-circular-progressbar';
-import { updateStart, updateSuccess, updateFailure } from "../redux/user/userSlice";
+import { useState, useRef, useEffect } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
-import 'react-circular-progressbar/dist/styles.css';
-
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
-  const [ imageFileUploadProgress, setImageFileUploadProgress ] = useState(null);
-  const [ imageFileError, setImageFileError ] = useState(null);
-  const [ imageFileUploading, setImageFileUploading ] = useState(false);
-  const [ updateUserSucess, setUpdateUserSuccess ] = useState(null);
-  const [ updateUserError, setUpdateUserError ] = useState(null);
-  const [ formData, setFormData ] = useState({});
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileError, setImageFileError] = useState(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [updateUserSucess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [formData, setFormData] = useState({});
   const fileRef = useRef();
   const dispatch = useDispatch();
   const handleImageChange = (e) => {
@@ -34,49 +42,50 @@ export default function DashProfile() {
       setImageFileError(null); // Reset error message in case of valid file size
     }
   };
-  
 
   useEffect(() => {
-    if (imageFile){
+    if (imageFile) {
       uploadImage();
     }
-  },[imageFile]);
+  }, [imageFile]);
 
   const uploadImage = async () => {
     // Reset or clear previous error messages
     setImageFileUploading(true);
     setImageFileError(null);
-  
+
     const storage = getStorage(app);
     const fileName = `${new Date().getTime()}${imageFile.name}`;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, imageFile);
-  
-    uploadTask.on('state_changed', 
+
+    uploadTask.on(
+      "state_changed",
       (snapshot) => {
         // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setImageFileUploadProgress(progress.toFixed(0));
-      }, 
+      },
       (error) => {
         // Handle unsuccessful uploads here
         setImageFileError("Could not upload image. Please try again.");
         setImageFileUploading(false);
-      }, 
+      },
       () => {
         // Handle successful uploads on complete
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           setImageFileUrl(downloadURL);
           setImageFileUploadProgress(null);
-          setFormData({...formData, profilePicture: downloadURL });
+          setFormData({ ...formData, profilePicture: downloadURL });
           setImageFileUploading(false);
         });
       }
     );
   };
-  
+
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value });
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
@@ -99,11 +108,10 @@ export default function DashProfile() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(!res.ok) {
+      if (!res.ok) {
         dispatch(updateFailure(data.message));
         setUpdateUserError(data.message);
-      }
-      else{
+      } else {
         dispatch(updateSuccess(data));
         setUpdateUserSuccess("User updated successfully");
       }
@@ -129,26 +137,28 @@ export default function DashProfile() {
         >
           {imageFileUploadProgress && (
             <CircularProgressbar
-                          value={imageFileUploadProgress || 0}
-                          text={`${imageFileUploadProgress}%`}
-                          styles={{
-                            root: {
-                              width: "100%",
-                              height: "100%",
-                            },
-                            path: {
-                              stroke: "#fff",
-                              strokeWidth: 4,
-                              transition: "stroke-dasharray 0.6s ease, stroke-dashoffset 0.6s ease",
-                            },
-                            trail: {
-                              stroke: "#fff",
-                              strokeWidth: 1,
-                              transition: "stroke-dasharray 0.6s ease, stroke-dashoffset 0.6s ease",
-                            },
-                          }}
-                        />
-                      )}
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                },
+                path: {
+                  stroke: "#fff",
+                  strokeWidth: 4,
+                  transition:
+                    "stroke-dasharray 0.6s ease, stroke-dashoffset 0.6s ease",
+                },
+                trail: {
+                  stroke: "#fff",
+                  strokeWidth: 1,
+                  transition:
+                    "stroke-dasharray 0.6s ease, stroke-dashoffset 0.6s ease",
+                },
+              }}
+            />
+          )}
           <img
             src={imageFileUrl || currentUser.profilePicture}
             alt="User Profile Picture"
@@ -156,14 +166,18 @@ export default function DashProfile() {
           ></img>
         </div>
 
-        {imageFileError && <Alert className="mt-5" color="failure">{imageFileError}</Alert>}
+        {imageFileError && (
+          <Alert className="mt-5" color="failure">
+            {imageFileError}
+          </Alert>
+        )}
 
         <TextInput
           type="text"
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
-          onChange={handleChange}             
+          onChange={handleChange}
         />
 
         <TextInput
@@ -171,14 +185,15 @@ export default function DashProfile() {
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
-          onChange={handleChange}                
+          onChange={handleChange}
         />
 
-        <TextInput 
-          type="password" 
-          id="password" 
+        <TextInput
+          type="password"
+          id="password"
           placeholder="Password"
-          onChange={handleChange} />
+          onChange={handleChange}
+        />
 
         <Button type="submit" gradientDuoTone="tealToLime" outline>
           Update
@@ -190,8 +205,16 @@ export default function DashProfile() {
         <span className="cursor-pointer">Sign Out</span>
       </div>
 
-      {updateUserSucess && <Alert className="mt-5" color="success">{updateUserSucess}</Alert>}
-      {updateUserError && <Alert className="mt-5" color="failure">{updateUserError}</Alert>}
+      {updateUserSucess && (
+        <Alert className="mt-5" color="success">
+          {updateUserSucess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert className="mt-5" color="failure">
+          {updateUserError}
+        </Alert>
+      )}
     </div>
   );
 }

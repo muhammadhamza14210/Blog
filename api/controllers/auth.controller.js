30405f1e05388d9121294e6a2f7f3da303359d4b
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-02-24 17:56:05 -0700

    Fixed some issues with regarding of displaying pic

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -10,20 +10,24 @@ export const signup = async (req, res, next) => {
     !username ||
     !email ||
     !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
+    username === '' ||
+    email === '' ||
+    password === ''
   ) {
-    next(errorHandler(400, "All fields are required"));
+    next(errorHandler(400, 'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
   try {
     await newUser.save();
-    res.json("Signed up successfully");
+    res.json('Signup successful');
   } catch (error) {
     next(error);
   }
@@ -32,32 +36,36 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
-  if (!email ||!password || email === "" || password === "") {
-    next(errorHandler(400, "All fields are required"));
+  if (!email || !password || email === '' || password === '') {
+    next(errorHandler(400, 'All fields are required'));
   }
 
   try {
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return next(errorHandler(404, "User not found"));
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, 'User not found'));
     }
-
-    const isMatch = bcryptjs.compareSync(password, user.password);
-
-    if (!isMatch) {
-      return next(errorHandler(400, "Invalid username or password"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, 'Invalid password'));
     }
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    const {password:pass, ...rest} = user._doc;
-    res.status(200).cookie('access_token', token,{
-      httpOnly: true}).json(rest);
-    next();
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const google = async (req, res, next) => {
   const { email, name, googlePhotoUrl } = req.body;

a257a635907749ed981ff448af5d4c0cbe05b93d
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-02-26 14:30:12 -0700

    fixed issue of sorting posts as per date

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,18 +1,18 @@
-import Post from '../models/post.model.js';
-import { errorHandler } from '../utils/error.js';
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to create a post'));
+    return next(errorHandler(403, "You are not allowed to create a post"));
   }
   if (!req.body.title || !req.body.content) {
-    return next(errorHandler(400, 'Please provide all required fields'));
+    return next(errorHandler(400, "Please provide all required fields"));
   }
   const slug = req.body.title
-    .split(' ')
-    .join('-')
+    .split(" ")
+    .join("-")
     .toLowerCase()
-    .replace(/[^a-zA-Z0-9-]/g, '');
+    .replace(/[^a-zA-Z0-9-]/g, "");
   const newPost = new Post({
     ...req.body,
     slug,
@@ -27,28 +27,29 @@ export const create = async (req, res, next) => {
 };
 
 export const getPosts = async (req, res, next) => {
-  try{
+  try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === 'asc'? 1 : -1;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
       ...(req.query.category && { category: req.query.category }),
-      ...(req.query.slug && {slug: req.query.slug }),
+      ...(req.query.slug && { slug: req.query.slug }),
       ...(req.query.postId && { _id: req.query.postId }),
-      ...(req.query.SearchTerm && {
-        $or:[
-          { title: { $regex: req.query.SearchTerm, $options: 'i' }},
-          { content: { $regex: req.query.SearchTerm, $options: 'i' }}
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
         ],
       }),
-    }).sort({ updateddAt: sortDirection }).skip(startIndex).limit(limit);
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
     const totalPosts = await Post.countDocuments();
 
     const now = new Date();
-    const oneMonthAgo = (
-      now.getFullYear(), now.getMonth()-1  , now.getDate()
-    );
+    const oneMonthAgo = (now.getFullYear(), now.getMonth() - 1, now.getDate());
 
     const lastMonthPosts = await Post.countDocuments({
       createddAt: {
@@ -56,7 +57,7 @@ export const getPosts = async (req, res, next) => {
       },
     });
     res.status(200).json({ posts, totalPosts, lastMonthPosts });
-  }catch (error) {
+  } catch (error) {
     next(error);
   }
 };

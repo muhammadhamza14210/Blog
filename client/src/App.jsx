7a9e53f3f827b78f99ad8fbefd8b3d7610b5acbd
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-02-26 20:38:03 -0700

    Add Update Post Functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'  
-import Footer from './components/Footer'
-import Header from './components/Header'
-import About from './pages/About'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import PrivateRoute from './components/PrivateRoute'
-import AdminPrivateRoute from './components/AdminPrivateRoute'
-import CreatePost from './pages/CreatePost'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
+import AdminPrivateRoute from "./components/AdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 export default function App() {
   return (
     <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path="/" element = {<Home/ >}></Route>
-            <Route path="/about" element = {<About/ >}></Route>
-            <Route path="/sign-in" element = {<SignIn/ >}></Route>
-            <Route path="/sign-up" element = {<SignUp/ >}></Route>
-            <Route element = {<PrivateRoute/>}>
-                <Route path="/dashboard" element = {<Dashboard/ >}></Route>
-            </Route>
-            <Route element = {<AdminPrivateRoute/>}>
-                <Route path="/create-post" element = {<CreatePost/ >}></Route>
-            </Route>
-            <Route path="/projects" element = {<Projects/ >}></Route>
-        </Routes>
-        <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+        </Route>
+        <Route element={<AdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />}></Route>
+          <Route path="/update-post/:postId" element={<UpdatePost />}></Route>
+        </Route>
+        <Route path="/projects" element={<Projects />}></Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }

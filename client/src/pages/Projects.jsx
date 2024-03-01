cfa8b40ee6d42de613fe6091417419499c711df6
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-03-01 15:38:48 -0700

    Completed Project section

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+        Looking to collaborate and make fun and exciting projects with me?
+      </p>
+      <CallToAction />
+    </div>
+  );
 }

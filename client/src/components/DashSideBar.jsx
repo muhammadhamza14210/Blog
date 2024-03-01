737088b2bbc15549acc9b25cf635260215105de1
Muhammad Hamza <muhammadhamza14210@gmail.com>
2024-03-01 12:31:27 -0700

    Completed dashboard overview for admin

--- client/src/components/DashSideBar.jsx
+++ client/src/components/DashSideBar.jsx
@@ -5,6 +5,7 @@ import {
   HiDocumentText,
   HiOutlineUserGroup,
   HiAnnotation,
+  HiChartPie,
 } from "react-icons/hi";
 import { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
@@ -43,6 +44,13 @@ export default function DashSideBar() {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup className="flex flex-col gap-1">
+          {currentUser && currentUser.isAdmin && (
+            <Link to="/dashboard?tab=dash">
+              <Sidebar.Item active={tab === "dash"} icon={HiChartPie} as="div">
+                DashBoard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
